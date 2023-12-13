f1d00300c25c2c79a336e5ec2e3a6cc0f40b5712
Tanner <tannerelephant@gmail.com>
2023-12-12 20:01:17 -0800

    add game input script

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+}
