c3821315692f28f46e7ce12b651e06cc0434a6f9
Tanner <tannerelephant@gmail.com>
2023-12-13 13:21:45 -0800

    add movement vector input into game input script

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -11,4 +11,11 @@ public class GameInput : MonoBehaviour
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
     }
+
+    private Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 movementVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+        movementVector = movementVector.normalized;
+        return movementVector;
+    }
 }
