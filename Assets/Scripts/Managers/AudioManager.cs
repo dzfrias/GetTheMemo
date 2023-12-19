188d70b19e0b3d8470f7abc2334846cb0cbb5a9b
Tanner <tannerelephant@gmail.com>
2023-12-18 16:35:03 -0800

    add music to audio manager

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -8,7 +8,10 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
     
-    [SerializeField] private List<Sound> sounds;
+    [SerializeField] private List<Sound> music;
+    [SerializeField] private List<Sound> soundEffects;
+
+    private AudioSource audioSource;
 
     [Serializable]
     public struct Sound
@@ -29,23 +32,34 @@ public class AudioManager : MonoBehaviour
         {
             Debug.LogError("There is more than 1 sound manager in the scene!");
         }
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void PlaySound(string name, Vector3 location)
     {
-        bool foundSound = false;
-        foreach (Sound sound in sounds)
+        Sound sound = GetSound(name, soundEffects);
+        AudioSource.PlayClipAtPoint(sound.audioClip, location, sound.volume);
+    }
+
+    public void SetMusic(string name)
+    {
+        Sound sound = GetSound(name, music);
+        audioSource.clip = sound.audioClip;
+        audioSource.volume = sound.volume;
+        audioSource.Play();
+    }
+
+    private Sound GetSound(string name, List<Sound> soundList)
+    {
+        foreach (Sound sound in soundList)
         {
             if (sound.name == name)
             {
-                AudioSource.PlayClipAtPoint(sound.audioClip, location, sound.volume);
-                foundSound = true;
+                return sound;
             }
         }
-
-        if (!foundSound)
-        {
-            Debug.LogError($"No Audio Clip Found with Name: '{name}'");
-        }
+        Debug.LogError($"No Audio Clip Found with Name: '{name}'");
+        return soundList[0];
     }
 }
