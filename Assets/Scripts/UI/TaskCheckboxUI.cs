e4eca1a99cad68ebea51dcb08601d6086d4e0105
Diego <96022404+dzfrias@users.noreply.github.com>
2023-12-21 11:55:05 -0800

    remove unused TaskCheckboxUI information

--- Assets/Scripts/UI/TaskCheckboxUI.cs
+++ Assets/Scripts/UI/TaskCheckboxUI.cs
@@ -4,16 +4,10 @@ using UnityEngine;
 
 public class TaskCheckboxUI : MonoBehaviour
 {
-    [SerializeField] private int taskID;
     [SerializeField] private GameObject checkmarkImage;
 
     public void CompleteTask()
     {
         checkmarkImage.SetActive(true);
-    }
-
-    public int GetTaskID()
-    {
-        return taskID;
     }
 }
