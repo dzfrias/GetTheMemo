25a1a0e0997c326714e283f053a3fb6d5fd06a42
Diego <96022404+dzfrias@users.noreply.github.com>
2024-02-20 20:44:17 -0800

    Improve recording mode editor script
    Make it automatically start the game (paused) and maximize the game
    window. It'll also begin recording.

--- Assets/Scripts/Editor/RecordingMode.cs
+++ Assets/Scripts/Editor/RecordingMode.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Recorder;
 using System.Linq;
 
 public class RecordingMode
@@ -16,5 +17,18 @@ public class RecordingMode
         {
             recorder.OnEnterRecordMode();
         }
+
+        // Start playing, pause, and maximize game view
+        EditorApplication.isPaused = true;
+        EditorApplication.isPlaying = true;
+        EditorWindow gameView = Resources
+                                    .FindObjectsOfTypeAll<EditorWindow>()
+                                    .Single(window => window != null && window.GetType().FullName == "UnityEditor.GameView");
+        gameView.maximized = true;
+        UnityEditorInternal.InternalEditorUtility.RepaintAllViews();
+
+        // Start recording
+        RecorderWindow recorderWindow = EditorWindow.GetWindow<RecorderWindow>();
+        recorderWindow.StartRecording();
     }
 }
