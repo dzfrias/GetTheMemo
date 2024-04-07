2d22e86c561ccf4a3777521b49b43f9fbdc057f9
Tanner <tannerelephant@gmail.com>
2024-04-07 07:50:08 -0700

    add basic combat bar effects

--- Assets/Scripts/UI/StaminaBarUI.cs
+++ Assets/Scripts/UI/StaminaBarUI.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.Tools;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class StaminaBarUI : MonoBehaviour
 {
-    [SerializeField]
-    private PlayerMovement playerMovement;
+    [SerializeField] private PlayerMovement playerMovement;
 
+
+    private MMProgressBar sprintProgressBar;
     private Image staminaBarImage;
     private float maxStaminaAmount;
 
     private void Awake()
     {
         staminaBarImage = GetComponent<Image>();
+        sprintProgressBar = GetComponent<MMProgressBar>();
     }
 
     private void Start()
@@ -24,6 +27,7 @@ public class StaminaBarUI : MonoBehaviour
 
     private void PlayerMovement_OnStaminaChanged(float amount)
     {
+        sprintProgressBar.UpdateBar(amount, 0, maxStaminaAmount);
         staminaBarImage.fillAmount = amount/maxStaminaAmount;
     }
 }
