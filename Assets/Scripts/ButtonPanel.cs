e94508547d782cdd170dfea5c8effd6752e43472
Diego <96022404+dzfrias@users.noreply.github.com>
2024-04-28 16:25:13 -0700

    Improve elevator transition

--- Assets/Scripts/ButtonPanel.cs
+++ Assets/Scripts/ButtonPanel.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains.Feedbacks;
 
 public class ButtonPanel : MonoBehaviour, IInteractable
 {
-    [SerializeField] private float delay = 3f;
     [SerializeField] private DialogueBox dialogueBox;
+
+    private MMF_Player player;
+    private bool didInteract;
+
+    private void Start()
+    {
+        player = GetComponent<MMF_Player>();
+    }
 
     public void Interact(Vector3 playerPos)
     {
+        if (didInteract) return;
+        didInteract = true;
         StartCoroutine(_GoToCombat());
+    }
+
+    public void _Finish()
+    {
+        GameManager.Instance.LoadScene(1);
     }
 
     private IEnumerator _GoToCombat()
     {
         dialogueBox.DisplayText("Going up.%1 Good luck out there,%0.5 intern.");
         yield return new WaitWhile(() => dialogueBox.IsPlaying());
-        yield return new WaitForSeconds(delay);
-        GameManager.Instance.LoadScene(1);
+        player.PlayFeedbacks();
     }
 }
